8387235d900da67f83e48b772d81edee77468913
JB-Tellez <jb.tellez@gmail.com>
2022-06-22 16:16:24 -0700

    returns json

--- api/photos.py
+++ api/photos.py
@@ -1,17 +1,28 @@
 from http.server import BaseHTTPRequestHandler
+import json
+
 
 class PhotoModel:
-    def __init__(self, id, photo_name, photo_url):
+    def __init__(
+        self, id, photo_name, photo_url="https://via.placeholder.com/200x200/ddd"
+    ):
         self.id = id
         self.photo_name = photo_name
         self.photo_url = photo_url
 
 
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        message = "Howdy Pardner"
+
+        photos = [
+            PhotoModel(1, "dog"),
+            PhotoModel(2, "cat"),
+            PhotoModel(3, "elephant"),
+        ]
+
+        message = json.dumps(photos)
+
         self.wfile.write(message.encode())
